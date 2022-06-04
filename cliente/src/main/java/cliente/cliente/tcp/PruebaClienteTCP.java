25895ce2ca5840b0d3df0b8b04246d990a18fb3f
Elias Beltran <eliasemilianobg@gmail.com>
2022-06-04 13:58:42 -0500

    Eliminando clases no utilizadas

--- cliente/src/main/java/cliente/cliente/tcp/PruebaClienteTCP.java
+++
@@ -1,7 +0,0 @@
-package cliente.cliente.tcp;
-
-public class PruebaClienteTCP{
-    public static void main(String args[])throws Exception{
-        ClienteTCP clienteTCP =new ClienteTCP("192.168.0.25",60000);
-    }
-}
